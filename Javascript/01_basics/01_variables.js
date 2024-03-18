a57d18ccfb13b7afaef7d4677f554760dced4305
Ali Zaidi <162324371+Aliasimshahmashwani677@users.noreply.github.com>
2024-03-18 19:42:39 +0500

    basics variables  JS

---
+++ Javascript/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_Id =1322
+let accountEmail = "asim@gmail.com"
+var accountPassword ="131234"
+
+accountCity = "swabi"
+
+//accountId =2  //  not allowed
+console.log("accountId");
+
+//better to not use  var because of scope issues
+accountPassword = "3644"
+var accountCity = "ghazi"
+console.log(accountId , accountEmail, accountPassword ,accountCity);
+//console.table([accountId])
