37e98c745763827b23a3323233f879b756a7d452
Ali Zaidi <162324371+Aliasimshahmashwani677@users.noreply.github.com>
2024-03-18 19:43:24 +0500

    datatypes  of JS

---
+++ datatypes/datatypes.js
@@ -0,0 +1,30 @@
+//"use strict " by writing this all our code is treated as modern new standards
+
+//alert(3+3) we are using node js not browser 
+
+//console.log(3+3); console.log("asim") //code must be readable
+
+
+//tc39 ecm documentation
+
+
+let name = "asim"
+let age  = 28
+let isLoggedIn = true
+
+
+//number 2 to power 53
+// bigint use 
+// string =>""  '' 
+//boolean  true or false
+//null standalone value
+ // undefined => value not defined variable with no value
+
+ // symbol =>uniqueness
+
+ // object
+
+ console.log(typeof null);
+ // typeof null is object
+
+
